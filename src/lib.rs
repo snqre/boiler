602f627ffa74de5c88528202e800172723235e11
snqre <marco.bizxaro@gmail.com>
2025-04-18 20:04:48 +0200

    *

---
+++ src/lib.rs
@@ -0,0 +1,36 @@
+/// Declares a module hierarchy from a given path using `automod`.
+///
+/// # Example
+/// ```rust,ignore
+/// bundle!("src/routes");
+/// ```
+///
+/// Requires the [`automod`](https://docs.rs/automod) crate.
+#[macro_export]
+macro_rules! bundle {
+    ($path:path) => {
+        automod::dir!($path);
+    };
+}
+
+/// Re-exports selected modules or items from a parent module into the current scope.
+///
+/// # Example
+/// ```rust,ignore
+/// public!(models, utils);
+/// ```
+///
+/// This expands to:
+/// ```rust,ignore
+/// pub use super::models::*;
+/// pub use super::utils::*;
+/// ```
+#[macro_export]
+macro_rules! public {
+    ( $( $module:ident ),* $(,)? ) => {
+        $(
+            #[allow(unused_imports)]
+            pub use super::$module::*;
+        )*
+    };
+}
